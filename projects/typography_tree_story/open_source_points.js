e83d3818bd1990c4142025b23816ccf9f1d88fe6
Valentine Wilson <valentinewilson@l33t-h4x0r.fios-router.home>
2020-06-16 13:52:04 -0400

    more responsive

--- projects/typography_tree_story/open_source_points.js
+++ projects/typography_tree_story/open_source_points.js
@@ -1,14 +1,20 @@
+
+var screen_width = $(document).width();
+var start =  80*screen_width/1250;
+var finish = screen_width-screen_width/100*17;
+var ran = Math.floor(finish-start);
 var svg = d3.select("#svg");
 var points_svg = d3.select("body").append("svg")
 .style('opacity',1)
 .attr("id", "points");
 var colors = ["rgb(201, 125, 190)","rgb(52, 117, 77)" ,"rgb(226, 111, 104)", "rgb(184,132,69)", "rgb(60,57,62)", "rgb(136,189,158)", "rgb(156,112,93)","rgb(201,109,28)","rgb(120, 90, 133)"];
-var size = [8,13,26,52,52,52,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,52,52,52,26,13]
-sum = 0;
-for(i in size){
-sum = sum + size[i];
-  for(j in [...Array(size[i]).keys()]){
-    place = parseInt(j);
+for(i in [...Array(Math.floor(ran)).keys()]){
+  i  = parseInt(i);
+  for(j in [...Array(Math.floor(ran)).keys()]){
+    j = parseInt(j);
+    cx = start +j*15;
+    cy = 8400+i*20;
+    if(i<25 && cx>start && cx<=finish+screen_width/50){
     svg.append("circle")
     .style('fill',function(f){
     color = colors[Math.floor(Math.random() * colors.length)];
@@ -18,15 +24,15 @@ sum = sum + size[i];
     .attr("class","oss" + toString(i))
     .style('stroke','rgb(0,0,0,0)')
     .style('opacity',function(f){
-      if(i>7){
-
-       return.03*i;
+      if(i>6){
+       return i*.03;
       }
       else{
         return .2;
       }
     })
-    .attr("cx",(1150*(place/size[i]))+103)
-    .attr('cy',8400+(i*15));
+    .attr("cx",cx)
+    .attr('cy',cy);
   }
+}
 }
